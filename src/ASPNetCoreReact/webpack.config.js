e83e796074acaa6a403705c511a9918ff3aa957e
Hon <honcre.c@gmail.com>
2018-05-17 16:37:00 +0700

    end with style-loader

--- src/ASPNetCoreReact/webpack.config.js
+++ src/ASPNetCoreReact/webpack.config.js
@@ -1,8 +1,36 @@
-﻿const path = require('path');
+﻿const path = require('path'); 
+const webpack = require('webpack');
+console.log(__dirname);//D:\Hon\NetCore\Yearzar\src\ASPNetCoreReact
+console.log(path.resolve(__dirname, 'wwwroot/dist'));//D:\Hon\NetCore\Yearzar\src\ASPNetCoreReact\wwwroot\dist
 module.exports = {
-    entry: './wwwroot/source/app-2.js',
+    entry: './wwwroot/source/app-3.js',
     output: {
         path: path.resolve(__dirname, 'wwwroot/dist'),
         filename: 'bundle.js'
-    }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',//object are list of Key:Value(dictionary)  access o["window.jQuery"]
+            Popper: ['popper.js', 'default'],
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{loader:"style-loader"},{ loader: "css-loader" }]
+            },
+            {
+            test: /\.js?$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets:['env'],
+                },
+            },
+            },
+        ],
+    },
 };
---
+++ src/ASPNetCoreReact/wwwroot/Source/app-3.js
@@ -0,0 +1,15 @@
+﻿//$ = require('jquery');
+require('./lib');
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/site.css';
+
+import ES6Lib from './es6codelib';
+
+document.getElementById('fillthis').innerHTML = getText();
+
+$("#fillthiswithjquery").html('Filled by Jquery!');
+
+let myES6Object = new ES6Lib();
+
+document.getElementById('fillthiswithes6lib').innerHTML = myES6Object.getData();
